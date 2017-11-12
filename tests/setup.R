ad37892e12b6351fb2e885cc28fd71b69df1ebe5
Anthony Damico <ajdamico@gmail.com>
2017-11-12 11:41:23 -0500

    enade bump

--- tests/setup.R
+++ tests/setup.R
@@ -1,4 +1,15 @@
 if ( .Platform$OS.type == 'windows' ) memory.limit( 256000 )
 
+this_sample_break <- Sys.getenv( "this_sample_break" )
+
 library(lodown)
-lodown( "enade" , output_dir = file.path( getwd() ) )
+
+enade_cat <-
+	get_catalog( "enade" ,
+		output_dir = file.path( getwd() ) )
+
+record_categories <- ceiling( seq( nrow( enade_cat ) ) / ceiling( nrow( enade_cat ) / 3 ) )
+
+enade_cat <- unique( rbind( enade_cat[ record_categories == this_sample_break , ] , enade_cat[ enade_cat$year == 2015 , ] ) )
+
+lodown( "enade" , enade_cat )
